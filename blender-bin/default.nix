0f9aa44a08b69f3921d33ff6d22ad97a91b63036
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-04-18 11:48:30 +0800

    blender-bin: blender_5_1: 5.1.0 -> 5.1.1

--- blender-bin/default.nix
+++ blender-bin/default.nix
@@ -212,10 +212,10 @@ in
 
   blender_5_1 = mkBlender {
     pname = "blender-bin";
-    version = "5.1.0";
+    version = "5.1.1";
     src = pkgs.fetchurl {
-      url = "https://download.blender.org/release/Blender5.1/blender-5.1.0-linux-x64.tar.xz";
-      hash = "sha256-fyR1mQYTyNTHrFaXgD/PQNCVQcH9jCOTb0sHoWmpIMc=";
+      url = "https://download.blender.org/release/Blender5.1/blender-5.1.1-linux-x64.tar.xz";
+      hash = "sha256-b5//if7xVO95dNGhxLkWq0vB9WGLy0jVvv7hvQp8fyo=";
     };
   };
 }
